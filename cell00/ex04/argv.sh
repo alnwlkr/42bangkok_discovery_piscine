20ad2a4f7a02dc38c669b9732b352f3e2398a011
Pongpada Anoma <poanoma@z2t7c3.42bangkok.com>
2022-09-01 18:43:59 +0700

    cell00 and cell01

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,17 @@
+if [ $# == 0 ]
+then
+	echo "No arguments supplied";
+else
+	if [ ! -z "$1" ]
+	then
+		echo $1
+	fi
+	if [ ! -z "$2" ]
+	then
+		echo $2
+	fi
+	if [ ! -z "$3" ]
+	then
+		echo $3
+	fi
+fi
---
+++ cell00/ex05/build.sh
@@ -0,0 +1,9 @@
+if [ $# == 0 ]
+then
+	echo "No arguments supplied"
+else
+	for i in $*
+	do
+		mkdir -p "ex$i"
+	done
+fi
